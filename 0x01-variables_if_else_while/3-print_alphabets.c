139b7814752b826e9f18f29fe587bb5874e9fa30
ukemeedidiong <ukemeedidiong226@gmail.com>
2023-08-11 15:09:51 -0700

    more of if

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar("\n");
+
+	result(0);
+}
