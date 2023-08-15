1238c00d3b63062694c24a4df95a86b28f61fec5
ukemeedidiong <ukemeedidiong226@gmail.com>
2023-08-15 09:21:49 -0700

    betty on beat

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char letters;
+	char letter;
 
 	while (count++ <= 9)
 	{
-		for (letters = 'a'; letters <= 'z'; letters++)
-			_putcharr(letters);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
