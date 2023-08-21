92a5e80abc3db5eab0f1f5057696f31b53993c05
ukemeedidiong <ukemeedidiong226@gmail.com>
2023-08-21 10:39:31 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - This function prints a reverse string
+ * @s: This is the input string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = i - 1; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  File: main.h
+ *  Auth: Nyebuk Sampson Inyang
+ *  Desc: Header file containing declarations for all functions
+ *  used in the 0x02-functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+
+#endif
